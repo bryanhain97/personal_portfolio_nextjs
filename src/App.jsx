1492a70a66ab29d4f820bdf5355d2c1911e8b184
Bryan Hain <bryan.hain@micromerce.com>
2022-02-01 10:28:43 +0100

    big changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,34 @@
-import React from 'react'
+import React, { useEffect } from 'react';
 import Navbar from './components/Navbar/Navbar.jsx';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from './components/Home/Home.jsx';
 
 function App() {
+    function toggleHidden() {
+        const text1Container = document.querySelector('#h1-text-container-1')
+        const text2Container = document.querySelector('#h1-text-container-2')
+        setInterval(() => {
+            text1Container.classList.toggle('hidden')
+            text2Container.classList.toggle('hidden')
+        }, 4000)
+    }
+    useEffect(toggleHidden, [])
     return (
-        <BrowserRouter>
+        <div className="app-container">
             <Navbar />
-            <Routes>
-                <Route path="/" element={<Home />} />
-                {/* <Route path="/about" component={About} />
-                <Route path="/contact" component={Contact} /> */}
-            </Routes>
-        </BrowserRouter >
+            <section id="home" className="section section1">
+                <div id="h1-text-container-1" className="h1-text-container">
+                    <h1 className="h1-text1">Hello. My name is Bryan.</h1>
+                    <h1 className="h1-text2">I'm a self taught developer.</h1>
+                </div>
+                <div id="h1-text-container-2" className="h1-text-container hidden">
+                    <h1 className="h1-text1">I'm 24 years old.</h1>
+                    <h1 className="h1-text2">from Berlin, Germany.</h1>
+                </div>
+            </section>
+            <section id="about" className="section section2">
+            </section>
+            <section id="contact" className="section section3">
+            </section>
+        </div>
     )
 }
 
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,22 +1,22 @@
 import React from 'react'
-import { Link } from 'react-router-dom';
-
+import { GrMenu } from 'react-icons/gr'
 
 function Navbar() {
     return (
-        <nav className="navbar-container">
-            <ul className="navbar-list">
-                <li className="navbar-list-element">
-                    <Link className="nav-list-link" to="/">Home</Link>
-                </li>
-                <li className="navbar-list-element">
-                    <Link className="nav-list-link" to="/about">About</Link>
-                </li>
-                <li className="navbar-list-element">
-                    <Link className="nav-list-link" to="/contact">Contact</Link>
-                </li>
-            </ul>
-        </nav>
+        <div className="navbar-container">
+            <nav className="navbar">
+                <ul className="navbar-list">
+                    <a href="#home"><li className="navbar-element">Home</li></a>
+                    <a href="#about"><li className="navbar-element">About</li></a>
+                    <a href="#contact"><li className="navbar-element">Contact</li></a>
+                </ul>
+            </nav>
+            <nav className="navbar-small">
+                <a href="#">
+                    <GrMenu className="navbar-small-icon" />
+                </a>
+            </nav>
+        </div>
     )
 }
 
