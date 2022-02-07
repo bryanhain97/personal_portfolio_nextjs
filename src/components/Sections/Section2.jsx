0d35a24be07d95479e8c1eafa489f33524007c13
Bryan Hain <bryan.hain@micromerce.com>
2022-02-07 23:39:59 +0100

    intersection observer

--- src/components/Sections/Section2.jsx
+++ src/components/Sections/Section2.jsx
@@ -1,12 +1,36 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 function Section2() {
-
+    let listItems;
+    let paragraphs;
+    useEffect(() => {
+        listItems = document.querySelectorAll('ul li.list-item');
+        paragraphs = document.querySelectorAll('.paragraph-container p')
+        console.log(listItems);
+        const options = {
+            rootMargin: '-150px 0px -30px 0px'
+        };
+        const observer = new IntersectionObserver((entries, observer) => {
+            entries.forEach(entry => {
+                if (!entry.isIntersecting) {
+                    return;
+                }
+                entry.target.classList.toggle('show', entry.isIntersecting);
+                observer.unobserve(entry.target);
+            })
+        }, options);
+        listItems.forEach(listItem => {
+            observer.observe(listItem);
+        })
+        paragraphs.forEach(paragraph => {
+            observer.observe(paragraph);
+        })
+    }, [])
     function selectTitle(e) {
         const allTitles = [...document.querySelectorAll('h2.section2-title')];
         e.target.parentNode.classList.add('selected');
         allTitles.forEach(title => {
-            if(allTitles.indexOf(title) !== allTitles.indexOf(e.target)){
+            if (allTitles.indexOf(title) !== allTitles.indexOf(e.target)) {
                 title.parentNode.classList.remove('selected');
             }
         })
@@ -22,13 +46,13 @@ function Section2() {
                         <p>I focus on the front-end, but can also build full-stack applications.</p>
                         <p>Technologies i commonly use include:</p>
                         <ul>
-                            <li>HTML</li>
-                            <li>CSS</li>
-                            <li>JavaScript</li>
-                            <li>MongoDB</li>
-                            <li>Express</li>
-                            <li>React</li>
-                            <li>NodeJS</li>
+                            <li className="list-item">HTML</li>
+                            <li className="list-item">CSS</li>
+                            <li className="list-item">JavaScript</li>
+                            <li className="list-item">MongoDB</li>
+                            <li className="list-item">Express</li>
+                            <li className="list-item">React</li>
+                            <li className="list-item">NodeJS</li>
                         </ul>
                     </div>
                 </div>
@@ -38,17 +62,16 @@ function Section2() {
                         <p>I specialize on web-development.</p>
                         <p>I help people find a fitting design for their needs and build eye-catching experiences.</p>
                         <p>I focus on the front-end, but can also build full-stack applications.</p>
-                        <p>Technologies i commonly use include:
+                        <p>Technologies i commonly use include:</p>
                         <ul>
-                                <li>HTML</li>
-                                <li>CSS</li>
-                                <li>JavaScript</li>
-                                <li>MongoDB</li>
-                                <li>Express</li>
-                                <li>React</li>
-                                <li>NodeJS</li>
-                            </ul>
-                        </p>
+                            <li className="list-item">HTML</li>
+                            <li className="list-item">CSS</li>
+                            <li className="list-item">JavaScript</li>
+                            <li className="list-item">MongoDB</li>
+                            <li className="list-item">Express</li>
+                            <li className="list-item">React</li>
+                            <li className="list-item">NodeJS</li>
+                        </ul>
                     </div>
                 </div>
                 <div className="section2-head-text head-facts">
@@ -57,17 +80,16 @@ function Section2() {
                         <p>I specialize on web-development.</p>
                         <p>I help people find a fitting design for their needs and build eye-catching experiences.</p>
                         <p>I focus on the front-end, but can also build full-stack applications.</p>
-                        <p>Technologies i commonly use include:
+                        <p>Technologies i commonly use include:</p>
                         <ul>
-                                <li>HTML</li>
-                                <li>CSS</li>
-                                <li>JavaScript</li>
-                                <li>MongoDB</li>
-                                <li>Express</li>
-                                <li>React</li>
-                                <li>NodeJS</li>
-                            </ul>
-                        </p>
+                            <li className="list-item">HTML</li>
+                            <li className="list-item">CSS</li>
+                            <li className="list-item">JavaScript</li>
+                            <li className="list-item">MongoDB</li>
+                            <li className="list-item">Express</li>
+                            <li className="list-item">React</li>
+                            <li className="list-item">NodeJS</li>
+                        </ul>
                     </div>
                 </div>
             </div>
