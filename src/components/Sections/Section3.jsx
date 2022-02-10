a1a3f8dde637603dcf740d865b0a7e52340c7a70
Bryan Hain <bryan.hain@micromerce.com>
2022-02-10 09:36:07 +0100

    section3

---
+++ src/components/Sections/Section3.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+// import icons from 'react-icons'
+
+function Section3() {
+    return (
+        <section id="contact" className="section section3">
+        </section>
+    )
+}
+
+export default Section3
