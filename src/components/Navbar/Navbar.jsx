dc014fe85314c598ca7e047a4ac91ee8b1ae77ea
Bryan Hain <bryan.hain@micromerce.com>
2022-02-13 11:32:36 +0100

    adjust styles

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -40,9 +40,9 @@ function Navbar({ menuOpen, setMenuOpen }) {
             <div className="navbar-container">
                 <nav className="navbar">
                     <ul className="navbar-list">
-                        <a href="#home"><li className="navbar-element">home</li></a>
-                        <a href="#about"><li className="navbar-element">about</li></a>
-                        <a href="#contact"><li className="navbar-element">contact</li></a>
+                        <a href="#home"><li className="navbar-element">Home</li></a>
+                        <a href="#about"><li className="navbar-element">About</li></a>
+                        <a href="#contact"><li className="navbar-element">Contact</li></a>
                     </ul>
                 </nav>
                 <nav className="navbar-small">
--- src/components/Sections/Section3.jsx
+++ src/components/Sections/Section3.jsx
@@ -23,23 +23,23 @@ function Section3() {
                 <div className="email-container">
                     <form className="form" ref={form} onSubmit={sendEmail}>
                         <label>Name</label>
-                        <input type="text" name="from_name" />
+                        <input type="text" name="from_name" required />
                         <label>Email</label>
-                        <input type="email" name="user_email" />
+                        <input type="email" name="user_email" required />
                         <label>Message</label>
-                        <textarea name="message" />
-                        <input type="submit" value="Send" />
+                        <textarea name="message" required/>
+                        <input className="input-submit" type="submit" value="Send" />
                     </form>
                 </div>
                 <div className="socials">
                     <div className="social-container">
                         <a href="https://github.com/bryanhain97" target="_blank" className="social"><TiSocialGithub className="social-icon" />GitHub</a>
-                        <a href="https://www.instagram.com/bry4n_h/" target="_blank" className="social"><TiSocialInstagram className="social-icon" />Instagram</a>
                         <a href="https://twitter.com/Bryan47588123" target="_blank" className="social"><TiSocialTwitter className="social-icon" />Twitter</a>
                         <a href="https://www.linkedin.com/in/bryan-hain-572568206/" target="_blank" className="social"><TiSocialLinkedin className="social-icon" />LinkedIn</a>
+                        <a href="https://www.instagram.com/bry4n_h/" target="_blank" className="social"><TiSocialInstagram className="social-icon" />Instagram</a>
                     </div>
                     <div className="img-container">
-                        <img src="./pictures/self.png" alt="selfie" />
+                        <img loading="lazy" src="./pictures/self.png" alt="selfie" />
                     </div>
                 </div>
             </div>
