1a640a5749be7f540f3d34a564260f71a153de66
Bryan Hain <bryan.hain@micromerce.com>
2022-02-26 19:38:51 +0100

    del server

--- server/server.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const app = express();
-require('dotenv').config();
-
-app.use(express.json())
-app.listen(3000, () => {
-    console.log('app listenting on port 3000')
-})
