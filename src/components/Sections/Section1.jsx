5dfc4cdc907b2a6f9449003a7c956539b8a856d9
Bryan Hain <bryan.hain@micromerce.com>
2022-03-31 12:55:31 +0100

    style updates

--- src/components/Sections/Section1.jsx
+++ src/components/Sections/Section1.jsx
@@ -42,11 +42,11 @@ function Section1() {
             {loaded && (
                 <>
                     <div id="h1-text-container-1" className="h1-text-container">
-                        <h1 className="h1-text1">Hello. My name is Bryan.</h1>
+                        <h1 className="h1-text1 h1-text-intro"><span id="hello">Hello</span>My name is Bryan.</h1>
                         <h1 className="h1-text2">I'm a self taught developer.</h1>
                     </div>
                     <div id="h1-text-container-2" className="h1-text-container hidden">
-                        <h1 className="h1-text1">I'm 24 years old.</h1>
+                        <h1 className="h1-text1 h1-text-intro">I'm 24 years old.</h1>
                         <h1 className="h1-text2">from Berlin, Germany.</h1>
                     </div>
                 </>)}
