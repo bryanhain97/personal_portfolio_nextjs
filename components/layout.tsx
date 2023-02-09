f70f20e4a339fb5f48304ebb52c9aec7d3d088c2
Bryan Hain <bryanhain97@gmail.com>
2023-02-09 15:46:18 +0100

    reset UI

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,16 +1,17 @@
 import { FC, ReactNode } from 'react'
 import Navbar from './Navbar'
 import styles from '../styles/partials/_Layout.module.scss'
-
+import { useColorMode } from '@chakra-ui/react'
 interface LAYOUT {
     children: ReactNode | ReactNode[]
 }
 
 const Layout: FC<LAYOUT> = ({ children }) => {
+    const { toggleColorMode } = useColorMode()
     return (
         <>
-            <div className={styles.layoutContainer}>
-                <Navbar />
+            <div className={styles.layoutContainer} onClick={() => toggleColorMode()}>
+                {/* <Navbar /> */}
                 <main className={styles.pageContent}>
                     {children}
                 </main>
